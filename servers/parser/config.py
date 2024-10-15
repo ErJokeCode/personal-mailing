9154b8114739b7f64f0e7c90c2eaaf32c243a8a1
nickpinecone <nick.pineconefir@gmail.com>
2024-10-15 22:01:59 +0500

    Integrate docker containers

--- servers/parser/config.py
+++ servers/parser/config.py
@@ -4,5 +4,5 @@ import os
 dotenv.load_dotenv()
 
 TOKEN_BOT = os.getenv('TOKEN_BOT')
-DB = Database(host="localhost", port=27017, database="personal-mailing", user_course_collection="user_course", 
+DB = Database(host="parserdb", port=27017, database="personal-mailing", user_course_collection="user_course", 
               course_info_collection="course_info", user_collection="user", user_auth_collection="user_auth")
--- servers/parser/main.py
+++ servers/parser/main.py
@@ -17,4 +17,4 @@ app.include_router(router=router_course)
 app.include_router(router=router_bot)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
