4d417b26a24c1d263ebe31b473273249dc6fbbe7
nickpinecone <nick.pineconefir@gmail.com>
2024-10-22 00:04:14 +0500

    Start work on the login system

--- servers/core/Models.cs
+++ servers/core/Models.cs
@@ -1,13 +1,19 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Shared.Core;
 
 namespace Core.Models;
 
-public class CoreDb : DbContext
+public class CoreDb : IdentityDbContext<ApplicationUser>
 {
     public DbSet<Student> Students => Set<Student>();
 
     public CoreDb(DbContextOptions<CoreDb> options) : base(options)
     {
     }
+}
+
+public class ApplicationUser : IdentityUser
+{
 }
--- servers/core/Program.cs
+++ servers/core/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Core.Models;
 using Shared.Messages;
 using MassTransit;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace Core;
 
@@ -34,12 +37,11 @@ public static class Program
                 options.AddDefaultPolicy(
                     policy =>
                     {
-                        policy.WithOrigins(new string[] { "http://localhost:5000" }).AllowAnyHeader().AllowAnyMethod();
+                        policy.WithOrigins(new string[] { "http://localhost:5010" }).AllowAnyHeader().AllowAnyMethod();
                     });
             });
 
-        var connection = builder.Configuration.GetConnectionString("Database");
-        builder.Services.AddDbContext<CoreDb>(options => options.UseNpgsql(connection));
+        builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
         builder.Services.AddMassTransit(o =>
                                         {
@@ -55,12 +57,19 @@ public static class Program
                                                                 cfg.ConfigureEndpoints(context);
                                                             });
                                         });
+
+        var connection = builder.Configuration.GetConnectionString("Database");
+        builder.Services.AddDbContext<CoreDb>(options => options.UseNpgsql(connection));
+
+        builder.Services.AddAuthorization(
+            options =>
+            { options.AddPolicy("CustomPolicy", policy => policy.RequireAuthenticatedUser()); });
+
+        builder.Services.AddIdentityApiEndpoints<ApplicationUser>().AddEntityFrameworkStores<CoreDb>();
     }
 
     public static async Task InitialzieServices(this WebApplication app)
     {
-        app.UseCors();
-
         using (var scope = app.Services.CreateScope())
         {
             var services = scope.ServiceProvider;
@@ -71,5 +80,9 @@ public static class Program
                 context.Database.Migrate();
             }
         }
+
+        app.UseCors();
+        app.MapIdentityApi<ApplicationUser>();
+        app.MapReverseProxy();
     }
 }
