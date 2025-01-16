13af90d70b2cc98fc820f8a28cdd3155cdf32aeb
ErJokeCode <eriksoloviev@gmail.com>
2025-01-16 10:35:20 +0500

    Update view infj about students

--- admin-panel/src/routes/student/SingleActive.svelte
+++ admin-panel/src/routes/student/SingleActive.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Breadcrumb, BreadcrumbItem, Heading } from 'flowbite-svelte';
+	import { Breadcrumb, BreadcrumbItem, Heading, Tabs, TabItem, Accordion, AccordionItem } from 'flowbite-svelte';
 	import { Button } from 'flowbite-svelte';
 	import { ArrowRightOutline } from 'flowbite-svelte-icons';
 
@@ -10,12 +10,15 @@
 
     export let id;
 
+    let main_student = {};
     let student = {};
     let status = http.status();
     let article;
 
     onMount(async () => {
-        student = await http.get(`/core/student/${id}`, status);
+        main_student = await http.get(`/core/student/${id}`, status) ?? {};
+        student = main_student.info !== undefined && main_student.info !== null ? main_student.info : {};
+        
         article.appendChild(traverseObject(student));
     });
 
@@ -39,15 +42,196 @@
                     <Link class="ml-0 ms-1 text-sm font-medium text-gray-700 hover:text-gray-900 md:ms-2 dark:text-gray-400 dark:hover:text-white" to="/students/active">Активные студенты</Link></li>
                     <BreadcrumbItem>Детали</BreadcrumbItem>
                 </Breadcrumb>
+                <Heading tag="h1" class="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl mb-3">
+                    {student.name} {student.surname} {student.patronymic}
+                </Heading>
                 <Button class="mb-3"
-                    on:click={() => start_chat(student.id)}>
+                    on:click={() => start_chat(main_student.id)}>
                     Начать чат
                     <ArrowRightOutline class="w-6 h-6 ms-2" />
                 </Button>
-                <Heading tag="h1" class="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl mb-3">
-                    Email: {student.email}
-                </Heading>
-                <div class="text-gray-900 dark:text-white" bind:this={article}></div>
+                <Tabs>
+                    <TabItem open title="Основная информация">
+                        <ul>
+                            <li>
+                                <p class="text-l text-gray-500 dark:text-gray-100 mb-2">
+                                    Номер студенческого билета:
+                                    {student.personal_number}
+                                </p>
+                            </li>
+
+                            <li>
+                                <p class="text-l text-gray-500 dark:text-gray-100 mb-2">
+                                    Почта:
+                                    {student.email}
+                                </p>
+                            </li>
+
+                            <li>
+                                <p class="text-l text-gray-500 dark:text-gray-100 mb-2">
+                                    Дата раждения:
+                                    {student.date_of_birth}
+                                </p>
+                            </li>
+
+                            <li>
+                                <p class="text-l text-gray-500 dark:text-gray-100 mb-2">
+                                    Группа:
+                                    {(student.group !== undefined) ? student.group.number : 'Не известен'}
+                                </p>
+                            </li>
+
+                            <li>
+                                <p class="text-l text-gray-500 dark:text-gray-100 mb-2">
+                                    Номер курса:
+                                    {(student.group !== undefined) ? student.group.number_course : 'Не известен'}
+                                </p>
+                            </li>
+
+                            <li>
+                                <p class="text-l text-gray-500 dark:text-gray-100 mb-2">
+                                    Направление:
+                                    {(student.group !== undefined) ? student.group.direction_code + " " + student.group.name_speciality : 'Не известно'}
+                                </p>
+                            </li>
+
+                            <li>
+                                <p class="text-l text-gray-500 dark:text-gray-100 mb-2">
+                                    Статус:
+                                    {student.status ? 'Активный студент' : 'Не активный студент'}
+                                </p>
+                            </li>
+
+                            <li>
+                                <p class="text-l text-gray-500 dark:text-gray-100 mb-2">
+                                    Тип оплаты обучения:
+                                    {student.type_of_cost}
+                                </p>
+                            </li>
+
+                            <li>
+                                <p class="text-l text-gray-500 dark:text-gray-100 mb-2">
+                                    Форма обучения:
+                                    {student.type_of_education}
+                                </p>
+                            </li>
+                        </ul>
+                    </TabItem>
+                    <TabItem title="Предметы">
+                        <Accordion>
+                            {#each student.subjects as subject}
+                                <AccordionItem>
+                                    <span slot="header">{subject.name}</span>
+                                    <ul>
+                                        <li>
+                                            <p class="text-l text-gray-500 dark:text-gray-100 mb-2">
+                                                <b>Полное название:</b>
+                                                {subject.full_name}
+                                            </p>
+                                        </li>
+
+                                        <li>
+                                            <p class="text-l text-gray-500 dark:text-gray-100 mb-2">
+                                                <b>Команды:</b>
+                                            </p>
+                                            {#if subject.teams !== undefined}
+                                                <ul style="margin-left: 30px">
+                                                    {#each subject.teams as team}
+                                                        <li>
+                                                            <p class="text-l text-gray-500 dark:text-gray-100 mb-2">
+                                                                <b>Номер:</b> {team.name}
+                                                            </p>
+                                                            <p class="text-l text-gray-500 dark:text-gray-100 mb-2">
+                                                                <b>Преподаватели:</b>
+                                                            </p>
+                                                            <ul>
+                                                                {#each team.teachers as teacher}
+                                                                    <li style="margin-left: 30px">
+                                                                        <p class="text-l text-gray-500 dark:text-gray-100 mb-2">
+                                                                            {teacher}
+                                                                        </p>
+                                                                    </li>
+                                                                {/each}
+                                                            </ul>
+                                                        </li>
+                                                    {/each}
+                                                </ul>
+                                            {/if}
+                                        </li>
+
+                                        <li>
+                                            <p class="text-l text-gray-500 dark:text-gray-100 mb-2">
+                                                <b>Форма обучения:</b>
+                                                {subject.form_education === "traditional" ? "Традиционное" : subject.form_education === "mixed" ? "Смешанное" : subject.form_education === "online" ? "Онлайн" : "Другое"}
+                                            </p>
+                                        </li>
+
+                                        <li>
+                                            <p class="text-l text-gray-500 dark:text-gray-100 mb-2">
+                                                <b>Группа в телеграмме:</b>
+                                                {subject.group_tg_link === null ? "Нет" : subject.group_tg_link}
+                                            </p>
+                                        </li>
+                                    </ul>
+                                </AccordionItem>
+                            {/each}
+                        </Accordion>
+                    </TabItem>
+                    <TabItem title="Онлайн курсы">
+                        <Accordion>
+                            {#each student.online_course as course}
+                                <AccordionItem>
+                                    <span slot="header">{course.name}</span>
+                                    <ul>
+                                        <li>
+                                            <p class="text-l text-gray-500 dark:text-gray-100 mb-2">
+                                                <b>Университет:</b>
+                                                {course.university}
+                                            </p>
+                                        </li>
+
+                                        <li>
+                                            <p class="text-l text-gray-500 dark:text-gray-100 mb-2">
+                                                <b>Дата начала:</b>
+                                                {course.date_start === null ? "Нет информации" : course.date_start}
+                                            </p>
+                                        </li>
+
+                                        <li>
+                                            <p class="text-l text-gray-500 dark:text-gray-100 mb-2">
+                                                <b>Дата конца:</b>
+                                                {course.deadline === null ? "Нет информации" : course.deadline}
+                                            </p>
+                                        </li>
+
+                                        <li>
+                                            <p class="text-l text-gray-500 dark:text-gray-100 mb-2">
+                                                <b>Дополнительная информация:</b>
+                                                {course.info === null ? "Нет информации" : course.info}
+                                            </p>
+                                        </li>
+
+                                        <li>
+                                            <p class="text-l text-gray-500 dark:text-gray-100 mb-2">
+                                                <b>Баллы:</b>
+                                            </p>
+                                        </li>
+
+                                        <ul style="margin-left: 30px">
+                                            {#each Object.keys(course.scores) as keys_score}
+                                                <li>
+                                                    <p class="text-l text-gray-500 dark:text-gray-100 mb-2">
+                                                        {keys_score}: {course.scores !== undefined ? course.scores[keys_score] : "Нет информации"}
+                                                    </p>
+                                                </li>
+                                            {/each}
+                                        </ul>
+                                    </ul>
+                                </AccordionItem>
+                            {/each}
+                        </Accordion>
+                    </TabItem>
+                </Tabs>
             </div>
         </div>
     </div>
