478bc5b057da0710e1cbdbf378b4cac3c95a0005
nickpinecone <nick.pineconefir@gmail.com>
2025-02-26 22:09:32 +0500

    Clean up comments

--- admin-panel2-revenge-of-the-backender/vite.config.js
+++ admin-panel2-revenge-of-the-backender/vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), tailwindcss()],
 
